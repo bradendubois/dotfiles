9893f27ad5cf570a562ecb6d0c8f66ca0d683671
Braden Dubois <braden.dubois@usask.ca>
2021-08-25 23:42:09 -0600

    refactored polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,11 +3,8 @@
 # Terminate already running bar instances
 killall -q polybar
 
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch bar
-echo "---" | tee -a /tmp/polybar.log
-polybar primary >>/tmp/polybar.log 2>&1
-
-echo "Bars launched."
+polybar primary --config="$HOME/.config/polybar/config.ini" > /tmp/polybar.log 2>&1 &
